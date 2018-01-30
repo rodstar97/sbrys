f7687e97f126d73c4a348c6e4335cb03e655ef45
rFriedrich <hermitmovie@googlemail.com>
2018-01-30 22:35:15 +0100

    refactor
    Signed-off-by: rFriedrich <hermitmovie@googlemail.com>

--- ui/qtd_main.py
+++ ui/qtd_main.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file '/home/rf/PycharmProjects/sbrys/ui/qtd_main.ui'
 #
-# Created: Tue Jan 30 22:25:42 2018
+# Created: Tue Jan 30 22:34:08 2018
 #      by: pyside-uic 0.2.15 running on PySide 1.2.4
 #
 # WARNING! All changes made in this file will be lost!
