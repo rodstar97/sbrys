bc215d261f9be7ad9b102ade9951d727ca2d6ae6
rFriedrich <hermitmovie@googlemail.com>
2018-02-16 12:13:11 +0100

    fix TabOrder
    Signed-off-by: rFriedrich <hermitmovie@googlemail.com>

--- sbrys_file_dialog.py
+++ sbrys_file_dialog.py
@@ -1,6 +1,6 @@
 from ui.fileDialog import Ui_create_sbrys_file
 from PySide import QtCore, QtGui
-
+from extra_widgets import FlowLayout
 tag_list = ['test','maya','houdini','mari','nuke']
 predefined_tag_list = ['maya', 'hypershade', 'shader']
 
@@ -15,6 +15,7 @@ class TagButton(QtGui.QPushButton):
 
         self.name = name
         self.setFixedHeight(20)
+        self.setMinimumWidth(70)
         self.setFocusPolicy(QtCore.Qt.NoFocus)
         self.setText(self.name)
         self.newfont = QtGui.QFont("Ubuntu", 7, QtGui.QFont.Normal)
@@ -27,6 +28,7 @@ class TagButton(QtGui.QPushButton):
         self.timer.timeout.connect(self.clicked.emit)
         self.clicked.connect(self.checkDoubleClick)
         self.doubleClicked.connect(self.remove_button)
+
 
     @QtCore.Slot()
     def checkDoubleClick(self):
@@ -41,6 +43,42 @@ class TagButton(QtGui.QPushButton):
         self.deleteLater()
 
 
+class SbrysLineEdit(QtGui.QLineEdit):
+    def __init__(self, *args, **kwargs):
+        QtGui.QLineEdit.__init__(self, *args, **kwargs)
+        self.multipleCompleter = None
+
+    def keyPressEvent(self, event):
+        QtGui.QLineEdit.keyPressEvent(self, event)
+        if not self.multipleCompleter:
+            return
+        c = self.multipleCompleter
+        if self.text() == "":
+            return
+        c.setCompletionPrefix(self.cursorWord(self.text()))
+        if len(c.completionPrefix()) < 1:
+            c.popup().hide()
+            return
+        c.complete()
+
+    def cursorWord(self, sentence):
+        p = sentence.rfind(" ")
+        if p == -1:
+            return sentence
+        return sentence[p + 1:]
+
+    def insertCompletion(self, text):
+        p = self.text().rfind(" ")
+        if p == -1:
+            self.setText(text)
+        else:
+            self.setText(self.text()[:p+1]+ text)
+
+    def setMultipleCompleter(self, completer):
+        self.multipleCompleter = completer
+        self.multipleCompleter.setWidget(self)
+        completer.activated.connect(self.insertCompletion)
+
 
 
 
@@ -50,11 +88,18 @@ class CreateSbrysFile(QtGui.QDialog):
         super(CreateSbrysFile, self).__init__()
         self.ui = Ui_create_sbrys_file()
         self.ui.setupUi(self)
+        self.tags_le = SbrysLineEdit()
+        self.tags_le.setObjectName('tags_le')
 
-
+        self.tags_flw = QtGui.QWidget()
+        self.tags_fl = FlowLayout()
+        self.tags_flw.setLayout(self.tags_fl)
+        self.ui.tag_buttons_hl.addWidget(self.tags_flw)
         #set auto completion in tags
         self.completer = QtGui.QCompleter()
-        self.ui.tags_le.setCompleter(self.completer)
+        self.tags_le.setMultipleCompleter(self.completer)
+
+
         self.model = QtGui.QStringListModel()
         self.completer.setModel(self.model)
         self.get_data(self.model)
@@ -62,28 +107,55 @@ class CreateSbrysFile(QtGui.QDialog):
         self.add_predifined_tags()
         self.ui.add_tag_pb.clicked.connect(self.add_tag_to_taglist)
         self.ui.toHide.hide()
+        self.ui.tags_hl.insertWidget(0, self.tags_le, 0)
         #self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setAutoDefault(False)
         self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setDefault(False)#setToolTip('Apply Tooltip')#button(QtGui.QDialogButtonBox.OK).setAutoFocus(False)
 
+        self.set_tab_order()
 
     def get_data(self, model, list=[]):
         #toDo filter out predifined Tags
         model.setStringList(tag_list)
 
+
     def add_predifined_tags(self, list=[]):
         for tag in predefined_tag_list:
-            self.ui.tag_hl.addWidget(TagButton(name=tag))
+            self.tags_fl.addWidget(TagButton(name=tag))
+
 
     def add_tag_to_taglist(self):
-        print 'ahha'
-        self.ui.tags_le.text()
-        self.ui.tag_hl.addWidget(TagButton(name=self.ui.tags_le.text()))
-        self.ui.tags_le.setText('')
+        #toDO better testing that no Tags with Spaces or emty Tags are allowed
+        if self.tags_le.text() != "":
+            for tag in self.tags_le.text().split(" "):
+                self.tags_fl.addWidget(TagButton(name=tag))
+                self.tags_le.setText('')
 
 
+    def set_tab_order(self):
+        """
+        name_le
+        tags_le
+        add_tag_pb
+        desr_le
+        desc_long_te
+        image_pb
 
-
-
+        :return:
+        """
+        self.tab_order_list = ['name_le','tags_le','add_tag_pb','desr_le','desc_long_te','image_pb']
+        self.short_name_obj_matrix={
+            'le':QtGui.QLineEdit,
+            'pb':QtGui.QPushButton,
+            'te':QtGui.QTextEdit
+        }
+        for index in xrange(len((self.tab_order_list))-1):
+            index_name = self.tab_order_list[index]
+            next_name =  self.tab_order_list[index+1]
+            index_widget = self.findChild(self.short_name_obj_matrix[index_name.split('_')[-1]], index_name)
+            next_widget  = self.findChild(self.short_name_obj_matrix[next_name.split('_')[-1]], next_name)
+            print index_widget, next_widget #widget =  eval(search_obj)
+            #print index , index +1
+            self.setTabOrder(index_widget, next_widget)
 
 
 if __name__ == '__main__':
